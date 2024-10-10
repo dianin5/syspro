d89b103f42a9a4ec363bc6aac30dfbc56a68d2b9
dianin5 <xyg1940@cu.ac.kr>
2024-10-10 00:29:57 +0000

    hw2 upload

--- chap5/hw2/main.c
+++ chap5/hw2/main.c
@@ -2,9 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcnl.h>
+#include <fcntl.h>
 
 #define BUFSIZE 1024
 
-int main(argc, char *argv[]){
+int main(int argc, char *argv[]){
+
+        int fd;
+        char saveText[BUFSIZE][BUFSIZE];
+        ssize_t nread;
+        char input[BUFSIZE];
+        int line_total=0;
+		char buf[1];
+		int i=0;
+		int j=0;
+        if((fd = open(argv[1], O_RDONLY)) ==-1){
+                perror(argv[1]);
+        exit(1);
+        }
+		while((nread=read(fd,buf,1)) > 0){
+                if(buf[0]=='\n'){
+                        saveText[i][j] = '\0';
+                        i++;
+                        j=0;
+                }
+                else {
+                        saveText[i][j] = buf[0];
+                        j++;
+                        }
+                }
+        line_total=i;
+        close(fd);
+		for(int i=line_total-1;i>=0;i--){
+			printf("%s\n",saveText[i]);
+		}
+		return 0;
+}
 	
