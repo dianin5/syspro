e98b090a3e55be5954cb24ef2eb45ba508d4fbd6
dianin5 <xyg1940@cu.ac.kr>
2024-10-20 08:33:29 +0000

    prob 11 -hw2

---
+++ chap6/prob11/main.c
@@ -0,0 +1,101 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <grp.h>
+#include <pwd.h>
+#include <string.h>
+
+char type(mode_t);
+char *perm(mode_t);
+void printStat(char*, char*,struct stat*, int,int,int);
+
+int main(int argc,char**argv){
+	DIR *dp;
+	char *dir=".";
+	struct stat st;
+	struct dirent *d;
+	char path[BUFSIZ+1];
+	int op_i=0, p=0,Q=0;
+
+	for (int i=1;i<argc;i++){
+		if(strcmp(argv[i], "-i")==0)
+			op_i=1;
+		else if(strcmp(argv[i], "-p")==0)
+			p =1;
+		else if(strcmp(argv[i], "-Q")==0)
+			Q =1;
+		else
+			dir = argv[i];
+	}
+
+
+	if((dp = opendir(dir)) == NULL)
+		perror(dir);
+
+	while((d = readdir(dp)) != NULL){
+		sprintf(path,"%s/%s", dir,d->d_name);
+		if(lstat(path,&st)<0)
+			perror(path);
+		else
+			printStat(path,d->d_name,&st,op_i,p,Q);
+	}
+	closedir(dp);
+	exit(0);
+}
+void printStat(char *pathname, char *file, struct stat *st,int op_i,int p,int Q){
+	if(op_i){
+		printf("ld% ", st->st_ino);
+	}
+	if(p){
+		printf("%c%s ", type(st->st_mode), perm(st->st_mode));
+	}
+
+	printf("%5d", st->st_blocks);
+	printf("%c%s ",type(st->st_mode), perm(st->st_mode));
+	printf("%3d",st-> st_nlink);
+	printf("%s %s",getpwuid(st->st_uid)->pw_name,getgrgid(st->st_gid)->gr_name);
+	printf("%9ld ", st->st_size);
+	printf("%.12s ", ctime(&st->st_mtime)+4);
+	if(Q){
+		printf("\"%s\"\n",file);
+	
+	}else{
+		printf("%s\n", file);
+	}
+}
+
+char type(mode_t mode){
+	if(S_ISREG(mode))
+		return('-');
+	if(S_ISDIR(mode))
+		return('d');
+	if(S_ISCHR(mode))
+		return('c');
+	if(S_ISBLK(mode))
+		return('b');
+	if(S_ISLNK(mode))
+		return('l');
+	if(S_ISFIFO(mode))
+		return('p');
+	if(S_ISSOCK(mode))
+		return('s');
+}
+
+char* perm(mode_t mode){
+	static char perms[10];
+	strcpy(perms, "--------");
+
+	for(int i=0;i < 3; i++){
+		if(mode & (S_IRUSR >> i*3))
+			perms[i*3] ='r';
+		if(mode & (S_IWUSR >> i*3))
+			perms[i*3+1]='w';
+		if(mode & (S_IXUSR >> i*3))
+			perms[i*3+2]='x';
+	}
+	return(perms);
+}
+
