ff2a4261026158924ec0993c90877a93e32ece65
dianin5 <xyg1940@cu.ac.kr>
2024-11-11 01:43:09 +0000

    chap9 end

---
+++ chap9/prob2/forkwait.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+   int pid, child, status;
+   printf("[%d] parent process start \n", getpid( ));
+   pid = fork();
+   if (pid == 0) {
+      printf("[%d] child process start \n", getpid( ));
+      exit(1);
+   }
+   child = wait(&status);
+   printf("[%d] child process %d end \n", getpid(), child);
+   printf("\texit code %d\n", status>>8);
+}
