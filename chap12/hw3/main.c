37786242969a1aed7fbeb9b0d5a404e3cdcdaccf
dianin5 <xyg1940@cu.ac.kr>
2024-12-04 02:51:45 +0000

    hw3 end

--- chap12/hw3/main.c
+++ chap12/hw3/main.c
@@ -1,53 +1,62 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
+#define READ 0
+#define WRITE 1
 
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <command1> <command2>\n", argv[0]);
+int main() {
+    char str[1024];
+    char *command1, *command2;
+    int fd[2];
+
+    printf("[shell] ");
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        return 0;
+    }
+
+    str[strlen(str) - 1] = '\0'; // '\n' 제거
+
+    if (strchr(str, '|') != NULL) {
+        command1 = strtok(str, "|");
+        command2 = strtok(NULL, "|");
+
+        if (command1 != NULL) {
+            command1 = strtok(command1, " "); // 명령어 정리
+        }
+        if (command2 != NULL) {
+            command2 = strtok(command2, " "); // 명령어 정리
+        }
+    } else {
+        fprintf(stderr, "Invalid command. Use the format: <command1> | <command2>\n");
+        return 1;
+    }
+
+    pipe(fd);
+
+    if (fork() == 0) { 
+        close(fd[READ]);
+        dup2(fd[WRITE], STDOUT_FILENO);
+        close(fd[WRITE]);
+        execlp(command1, command1, NULL);
+        perror("execlp");
         exit(1);
     }
 
-    int pipefd[2];
-    pid_t pid1, pid2;
-
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
+    if (fork() == 0) { 
+        close(fd[WRITE]);
+        dup2(fd[READ], STDIN_FILENO);
+        close(fd[READ]);
+        execlp(command2, command2, NULL);
+        perror("execlp");
         exit(1);
     }
 
-    pid1 = fork();
-    if (pid1 == 0) {
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
-
-        char *cmd1[] = {"/bin/sh", "-c", argv[1], NULL};
-        execv("/bin/sh", cmd1);
-        perror("execv");
-        exit(1);
-    }
-
-    pid2 = fork();
-    if (pid2 == 0) {
-        close(pipefd[1]);
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
-
-        char *cmd2[] = {"/bin/sh", "-c", argv[2], NULL};
-        execv("/bin/sh", cmd2);
-        perror("execv");
-        exit(1);
-    }
-
-    close(pipefd[0]);
-    close(pipefd[1]);
-
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
+    close(fd[READ]);
+    close(fd[WRITE]);
+    wait(NULL);
+    wait(NULL);
 
     return 0;
 }
